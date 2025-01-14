13443602a676284a0f3da640cfbdd8f5eeb0d50d
Juhee Dhone <juheedhone12@gmail.com>
2025-01-14 18:55:39 +0530

    Ship genres and platforms with the app.

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,10 @@
-import useData from "./useData";
-
+import platform from "../Data/platform";
 interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: platform, error: null });
 
 export default usePlatforms;
